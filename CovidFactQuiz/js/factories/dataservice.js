b8f94daa6f9cec43455aad2ba9042dde08b4c8d4
RoshanMishra07 <68501175+RoshanMishra07@users.noreply.github.com>
2020-11-05 08:51:17 +0530

    Update dataservice.js

--- CovidFactQuiz/js/factories/dataservice.js
+++ CovidFactQuiz/js/factories/dataservice.js
@@ -1,25 +1,13 @@
-/*
- * IIFE to keep code safe and outside the global namespace
- */
+
 (function(){
 
-    /*
-     * Declaring a factory service as part of the existing covidFacts Module.
-     */
     angular
         .module("covidFacts")
         .factory("DataService", DataService);
 
-    /*
-     * Actual definition of the function used for this factory
-     */
+
     function DataService(){
-        /*
-         * dataObj is used to simulate getting the data from a backend server
-         * The object will hold data which will then be returned to the other
-         * factory declared in js/factory/quiz.js which has this factory
-         * as a dependency
-         */
+
 
         var dataObj = {
             covidData: covidData,
@@ -27,40 +15,10 @@
             correctAnswers: correctAnswers
         };
 
-        // returning the dataObj to anything that uses this factory as a
-        // dependency
+
         return dataObj;
     }
 
-    /*
-     * all of the below variables are simulating data that would typically be
-     * retrieved using an HTTP call to an API endpoint.
-     *
-     * For simplicity sake this data is hardcoded into the app as this tutorial
-     * is about building the angular app, not the backend.
-     *
-     * The correctAnswers variable would be retrieved when the user has
-     * finished the quiz and would be used to mark the users answers against
-     * the correct answers
-     *
-     * the quizQuestions is an array of objects, each containing data
-     * pertaining to a single question. This includes:
-     *                          - The type of question: image or text
-     *                          - Text of the question (aka the actual question)
-     *                          - A set of 4 possible answers, either text or
-     *                              images as indicated above
-     *                          - a selected flag which will be used to know if
-     *                              the user has selected
-     *                          an answer yet.
-     *                          - Whether the user got the question correct or
-     *                              not
-     *
-     * The final covidData variable hold the information that will be
-     * displayed in the list view when the app loads. This includes the name
-     * and an image of each covid as well as other information such as the
-     * location and the size of the covids
-     *
-     */
 
     var correctAnswers = [1, 1, 1, 2, 0, 0, 3, 0, 3, 2];    
 
