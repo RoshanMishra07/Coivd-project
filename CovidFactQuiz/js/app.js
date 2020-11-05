4fd7ff5d283f9e1e4514d3527b4c1ec15fdf14a2
RoshanMishra07 <68501175+RoshanMishra07@users.noreply.github.com>
2020-11-05 08:50:29 +0530

    Update app.js

--- CovidFactQuiz/js/app.js
+++ CovidFactQuiz/js/app.js
@@ -1,14 +1,7 @@
-/*
- * IIFE to keep code clean and safe
- */
+
 (function(){
 
-    /*
-     * Declaration of main angular module for this appllication.
-     *
-     * It is named covidFacts and has no dependencies (hence the 
-     * empty array as the second argument)
-     */
+
     angular
         .module("covidFacts", []);
 
